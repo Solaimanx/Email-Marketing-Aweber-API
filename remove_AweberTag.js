8188e2e701de839b2ad9834ae05ee07f78cde2a6
Mohammad Solaiman <solaiman321@gmail.com>
2022-01-07 01:48:30 +0600

    catch error with the try catch funtion g

--- remove_AweberTag.js
+++ remove_AweberTag.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
-const removeTag = async(email,accessToken,tag) => {
-  const accountId = 1025920
-  const listId = 5578186
+const removeTag = async (email, accessToken, tag) => {
+  const accountId = 1025920;
+  const listId = 5578186;
 
   const headers = {
     Accept: "application/json",
@@ -12,38 +12,24 @@ const removeTag = async(email,accessToken,tag) => {
 
   const data = JSON.stringify({
     tags: {
-        "remove": [`${tag}`]
+      remove: [`${tag}`],
     },
   });
-
 
   const config = {
     method: "PATCH",
     url: `https://api.aweber.com/1.0/accounts/${accountId}/lists/${listId}/subscribers?subscriber_email=${email}`,
     headers: headers,
-    data: data
+    data: data,
+  };
+
+  try {
+    const response = await axios(config);
+    const status = response.status;
+    return status;
+  } catch (error) {
+    console.log(error);
   }
-
-  const response = await axios(config)
-  const status = response.status
-  return status
-
-  // .then(function (response) {
-    //   console.log(response.status); /// 209 is expected
-    //   return response.status
-
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-
-
-
-
-
-
-
-
 };
 
 module.exports = { removeTag };
