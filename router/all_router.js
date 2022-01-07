3f236b06dabe701a6292964b06b6fe85ed500a72
Mohammad Solaiman <solaiman321@gmail.com>
2022-01-07 22:26:16 +0600

    added remove clickfunnel tag scheduling system

--- router/all_router.js
+++ router/all_router.js
@@ -5,25 +5,36 @@ const Token = mongoose.model("token");
 const RemoveClickfunnelTag = require("../remove_ClickfunnelTag");
 const Aweber = require("../remove_AweberTag");
 const tradeRefreshToken = require("../utils/trade_refresh_token");
-
+const schedule = require("node-schedule");
 
 router.post("/webhook", (req, res) => {
-
-
   const proTag = "pro";
   const basicTag = "basic";
   const englishByTheWayTag = "english";
   const id = "61c63785bcb637c1cb9e6270";
   const { client_email, course_name } = req.body;
 
-
   if (course_name == "קונפידנס") {
     ///pro basic tag
 
     RemoveClickfunnelTag.Tag(basicTag, client_email);
     RemoveClickfunnelTag.Tag(proTag, client_email);
-    // RemoveClickfunnelTag.Tag(basicTag, client_email);
-    // RemoveClickfunnelTag.Tag(proTag, client_email);
+
+    const twomins = new Date().getTime() + 2 * 60 * 1000;
+    const dateTwo = new Date(twomins);
+
+    const fourmins = new Date().getTime() + 4 * 60 * 1000;
+    const dateFour = new Date(fourmins);
+
+    schedule.scheduleJob(dateTwo, function () {
+      RemoveClickfunnelTag.Tag(basicTag, client_email);
+      RemoveClickfunnelTag.Tag(proTag, client_email);
+    });
+
+    schedule.scheduleJob(dateFour, function () {
+      RemoveClickfunnelTag.Tag(basicTag, client_email);
+      RemoveClickfunnelTag.Tag(proTag, client_email);
+    });
 
     //get old token
     Token.findById(id)
@@ -50,11 +61,11 @@ router.post("/webhook", (req, res) => {
             }
             const { accessToken } = result;
             const tag = "buyer";
-            const response = await Aweber.removeTag(client_email, accessToken, tag);
-            RemoveClickfunnelTag.Tag(proTag, client_email);
-            RemoveClickfunnelTag.Tag(basicTag, client_email);
-            // RemoveClickfunnelTag.Tag(proTag, client_email);
-            // RemoveClickfunnelTag.Tag(basicTag, client_email);
+            const response = await Aweber.removeTag(
+              client_email,
+              accessToken,
+              tag
+            );
             if (response == 209) {
               return res.status(200).json({ message: "success" });
             } else {
@@ -66,8 +77,6 @@ router.post("/webhook", (req, res) => {
       .catch((err) => {
         console.log("get access token Error: " + err);
       });
-
-      
   } else if (course_name == "English By The Way") {
     ///English by the way tag
     RemoveClickfunnelTag.Tag(englishByTheWayTag, client_email);
@@ -97,7 +106,11 @@ router.post("/webhook", (req, res) => {
             }
             const { accessToken } = result;
             const tag = "english by the way";
-            const response = await Aweber.removeTag(client_email, accessToken, tag);
+            const response = await Aweber.removeTag(
+              client_email,
+              accessToken,
+              tag
+            );
             RemoveClickfunnelTag.Tag(englishByTheWayTag, client_email);
             RemoveClickfunnelTag.Tag(englishByTheWayTag, client_email);
             if (response == 209) {
@@ -114,30 +127,22 @@ router.post("/webhook", (req, res) => {
   }
 });
 
-
-router.get('/show-token',(req, res) => {
-
-  console.log(req.query)
+router.get("/show-token", (req, res) => {
+  console.log(req.query);
 
   const id = "61c63785bcb637c1cb9e6270";
 
-
   Token.findById(id)
-  .select("accessToken  -_id")
-  .then(async (token) => {
-    const { accessToken } = token;
-    return res.status(200).json({accessToken})
-}).catch((err) => {
-      if(err){
-          return res.status(200).json({ message: err})
-
+    .select("accessToken  -_id")
+    .then(async (token) => {
+      const { accessToken } = token;
+      return res.status(200).json({ accessToken });
+    })
+    .catch((err) => {
+      if (err) {
+        return res.status(200).json({ message: err });
       }
-  })
-
-
-})
-
-
-
+    });
+});
 
 module.exports = router;
